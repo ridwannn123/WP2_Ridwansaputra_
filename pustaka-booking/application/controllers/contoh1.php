2fda9eb815486247f7b1cb6f0eb41e33edb95629
ridwannn123 <ridwannnn.saputra24@gmail.com>
2022-03-22 16:17:25 +0700

    pustaka-booking

---
+++ pustaka-booking/application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_CONTROLLER
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Ridwan Saputra
+              Saya tinggal di Kemayoran
+              olahraga yang di sukai badminton.";
+    }
+}
+?>
