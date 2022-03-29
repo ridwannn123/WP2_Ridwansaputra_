037c19132d0c084457f38550d27230b97a40648b
ridwannn123 <ridwannnn.saputra24@gmail.com>
2022-03-29 20:42:51 +0700

    penambahan ada di README

---
+++ pustaka-booking/application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Ridwan</li>
+        <li>Nama Belakang : Saputra</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Dakota II Kemayoran </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Lampung</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Badminton</li>
+        <li>Futsal</li>
+    </ul>
+</section>
+
