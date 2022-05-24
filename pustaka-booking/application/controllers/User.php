4e4e1f97798f736b74de2060a6dfe6a092e9d6fa
ridwannn123 <ridwannnn.saputra24@gmail.com>
2022-05-24 10:04:56 +0700

    penambahan ada di README

--- pustaka-booking/application/controllers/User.php
+++ pustaka-booking/application/controllers/User.php
@@ -1,17 +1,14 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class User extends CI_Controller
-{
-    public function __construct()
-    {
+class User extends CI_Controller {
+    public function __construct() {
         parent::__construct();
         cek_login();
     }
-    
-    public function index()
-    {
-        $data['judul'] = 'Profil Saya';
+
+    public function index() {
+        $data['judul'] = 'Profile Saya';
         $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
 
         $this->load->view('templates/header', $data);
@@ -21,72 +18,69 @@ class User extends CI_Controller
         $this->load->view('templates/footer');
     }
 
-    public function anggota()
-    {
+    public function anggota() {
         $data['judul'] = 'Data Anggota';
         $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
         $this->db->where('role_id', 1);
         $data['anggota'] = $this->db->get('user')->result_array();
-    
+        $data['buku'] = $this->ModelBuku->getBuku()->result_array();
+
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);
-        $this->load->view('user/anggota', $data);
+        $this->load->view('admin/index', $data);
         $this->load->view('templates/footer');
     }
 
-    public function ubahProfil()
-    {
+    public function ubahProfil() {
         $data['judul'] = 'Ubah Profil';
         $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
-    
-        $this->form_validation->set_rules('nama', 'Nama Lengkap',
-'required|trim', [
-            'required' => 'Nama tidak Boleh Kosong'
-    ]);
 
-    if ($this->form_validation->run() == false) {
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar', $data);
-        $this->load->view('user/ubah-profile', $data);
-        $this->load->view('templates/footer');
-    } else {
-        $nama = $this->input->post('nama', true);
-        $email = $this->input->post('email', true);
+        $this->form_validation->set_rules('nama', 'Nama Lengkap', 'required|trim', [
+            'required' => 'Nama tidak boleh kosong'
+        ]);
         
-        //jika ada gambar yang akan diupload
-        $upload_image = $_FILES['image']['name'];
-        
-        if ($upload_image) {
-            $config['upload_path'] = './assets/img/profile/';
-            $config['allowed_types'] = 'gif|jpg|png';
-            $config['max_size'] = '3000';
-            $config['max_width'] = '1024';
-            $config['max_height'] = '1000';
-            $config['file_name'] = 'pro' . time();
-        
-            $this->load->library('upload', $config);
-        
-            if ($this->upload->do_upload('image')) {
-                $gambar_lama = $data['user']['image'];
-                if ($gambar_lama != 'default.jpg') {
-                    unlink(FCPATH . 'assets/img/profile/' .
-$gambar_lama);
-                }
-                $gambar_baru = $this->upload->data('file_name');
-                $this->db->set('image', $gambar_baru);
-            } else { }
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/ubah_profile', $data);
+            $this->load->view('templates/footer', $data);
+        } else {
+            $nama = $this->input->post('nama', true);
+            $email = $this->input->post('email', true);
+
+            //jika ada gambar yang akan diupload
+            $upload_image = $_FILES['image']['name'];
+
+            if ($upload_image) {
+                $config['upload_path'] = './assets/img/profile/';
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size'] = '3000';
+                $config['max_width'] = '1024';
+                $config['max_height'] = '1000';
+                $config['file_name'] = 'pro' . time();
+
+                $this->load->library('upload', $config);
+                $this->upload->initialize($config);
+
+                if ($this->upload->do_upload('image')) {
+                    $gambar_lama = $data['user']['image'];
+                    if ($gambar_lama != 'default.jpg') {
+                        unlink(FCPATH . 'assets/img/profile/' . $gambar_lama);
+                    }
+
+                    $gambar_baru = $this->upload->data('file_name');
+                    $this->db->set('image', $gambar_baru);
+                } 
+            }
+
+            $this->db->set('nama', $nama);
+            $this->db->where('email', $email);
+            $this->db->update('user');
+
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Profil Berhasil diubah </div>');
+            redirect('user');
         }
-        
-        $this->db->set('nama', $nama);
-        $this->db->where('email', $email);
-        $this->db->update('user');
-        
-        $this->session->set_flashdata('pesan', '<div
-class="alert alert-success alert-message" role="alert">Profil
-Berhasil diubah </div>');
-        redirect('user');
     }
-  }
 }
--- pustaka-booking/application/views/user/ubah_profile.php
+++ pustaka-booking/application/views/user/ubah_profile.php
@@ -1,20 +1,16 @@
-<!-- Begin Page Content -->
 <div class="container-fluid">
- 
     <div class="row">
         <div class="col-lg-9">
             <?= form_open_multipart('user/ubahprofil'); ?>
             <div class="form-group row">
-                <label for="email" class="col-sm-2 col-formlabel">Email</label>
+                <label for="email" class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control" id="email" name="email" value="<?= $user['email']; ?>" readonly>
                 </div>
-            </div>
-            <div class="form-group row">
-                <label for="nama" class="col-sm-2 col-formlabel">Nama Lengkap</label>
+                <label for="nama" class="col-sm-2 col-form-label">Nama Lengkap</label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control" id="nama" name="nama" value="<?= $user['nama']; ?>">
-                    <?= form_error('nama', '<small class="textdanger pl-3">', '</small>'); ?>
+                    <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
             </div>
             <div class="form-group row">
@@ -22,32 +18,26 @@
                 <div class="col-sm-10">
                     <div class="row">
                         <div class="col-sm-3">
-                            <img src="<?=
-base_url('assets/img/profile/') . $user['image']; ?>" class="imgthumbnail" alt="">
+                            <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-thumbnail" alt="">
                         </div>
                         <div class="col-sm-9">
                             <div class="custom-file">
-                                <input type="file" class="customfile-input" id="image" name="image">
-                                <label class="custom-file-label" for="image">Pilih file</label>
+                                <input type="file" class="custom-file-input" id="image" name="image">
+                                <label for="image" class="custom-file-label">Pilih file</label>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
- 
             <div class="form-group row justify-content-end">
                 <div class="col-sm-10">
-                    <button type="submit" class="btn btnprimary">Ubah</button>
+                    <button type="submit" class="btn btn-primary">Ubah</button>
                     <button class="btn btn-dark" onclick="window.history.go(-1)"> Kembali</button>
                 </div>
             </div>
- 
+
             </form>
         </div>
     </div>
-
 </div>
-<!-- /.container-fluid -->
-
 </div>
-<!-- End of Main Content -->
